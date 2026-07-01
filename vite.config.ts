2f91af79e4ae7f777f25280b4e254b760c82a73f
mcfadyentheresa-lab <mcfadyentheresa@gmail.com>
2026-06-30 22:58:24 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
-  optimizeDeps: {
-    exclude: ["lucide-react"],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "index";
+          }
+        },
+      },
+    },
   },
 });
